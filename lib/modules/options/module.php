18f0291f1ed4dac9822bba6d1743cbd347beb768
Bryan Haskin <bhhaskin@users.noreply.github.com>
2014-11-26 17:02:12 -0800

    updated core

---
+++ lib/modules/options/module.php
@@ -0,0 +1,15 @@
+<?php
+namespace themeOptions;
+
+require_once get_template_directory() . '/lib/modules/core/page.class.php';
+
+class Options extends Page{
+
+  public function init() {
+
+  }
+
+
+    }
+
+    new Options();
